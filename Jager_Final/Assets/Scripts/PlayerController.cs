4781bf3bc3377b4490264c87f12e7572dc8e08e0
njager <nicholasejager@gmail.com>
2020-11-14 16:46:08 -0500

    added variables and uses to player script

--- Jager_Final/Assets/Scripts/PlayerController.cs
+++ Jager_Final/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
+    //member variables
+    private NavMeshAgent agent;
+    private RaycastHit clickInfo = new RaycastHit();
+
     // Start is called before the first frame update
     void Start()
     {
