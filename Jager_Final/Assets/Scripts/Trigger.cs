2627b051486526d01dead55421f45876905574bb
njager <nicholasejager@gmail.com>
2020-11-15 18:08:48 -0500

    populated trigger script with variables

--- Jager_Final/Assets/Scripts/Trigger.cs
+++ Jager_Final/Assets/Scripts/Trigger.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+    //public variables
+    public GameObject target;
+    public string triggerVariable;
+
+    //private variables
+    private PlayerController player;
+    private Animator targetAnimator;
+    private bool isPlayerRestored;
+
     // Start is called before the first frame update
     void Start()
     {
