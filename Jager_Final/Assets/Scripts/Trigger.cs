b48733f3da0d04bc06fd06f7ae925b815d0f3380
njager <nicholasejager@gmail.com>
2020-11-15 18:17:09 -0500

    added parenting and movement functions to trigger scriot

--- Jager_Final/Assets/Scripts/Trigger.cs
+++ Jager_Final/Assets/Scripts/Trigger.cs
@@ -25,6 +25,32 @@ public class Trigger : MonoBehaviour
         }
     }
 
+    //toggles the animator parameter and parents player to moving platform
+    void TriggerAction()
+    {
+        bool targetVar = targetAnimator.GetBool(triggerVariable);
+        targetAnimator.SetBool(triggerVariable, !targetVar);
+
+        player.EnableCleanMove(target.transform);
+        isPlayerRestored = false;
+    }
+
+    //reverses trigger aciton
+    void RestorePlayer()
+    {
+        player.DisableCleanMove();
+        isPlayerRestored = true;
+    }
+
+    //when player enters trigger, call trigger action
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TriggerAction();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
